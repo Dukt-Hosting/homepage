269c6b63f32ce006806517827ace21fb4e3234e7
Joshua Ellis <67237931+SpicyTakis@users.noreply.github.com>
2021-02-05 09:12:14 -0500

    Better error page ig?
    Added css to the error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,9 +1,23 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import NavBar from '../components/NavBar.js'
 
 function ErrorPage({ Component, pageProps }) {
   return (
-      <h1>
-        Something went wrong :c, please try again later.
-      </h1>
+      <div style={styles.container
+          <Head>
+              <title>Home - Dukt Hosting</title>
+              <link rel="icon" href="/favicon.ico" />
+          </Head>
+    
+          <NavBar brand="Dukt Hosting"></NavBar>
+    
+          <Main className={styles.main}>
+              <h2>
+                  An error occured in your request, try again later.
+              <h3>
+          </Main>
+      </div>
   )
 }
 
