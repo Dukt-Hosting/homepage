3660028aa71e1d5adfbe31c85f187b845140e8e9
Joshua Ellis <67237931+SpicyTakis@users.noreply.github.com>
2021-02-05 09:14:06 -0500

    Better 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,23 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import NavBar from '../components/NavBar.js'
+
 function FourOFourPage({ Component, pageProps }) {
   return (
-      <h1>
-        404 not found.
-      </h1>
+      <div style={styles.container
+          <Head>
+              <title>Home - Dukt Hosting</title>
+              <link rel="icon" href="/favicon.ico" />
+          </Head>
+    
+          <NavBar brand="Dukt Hosting"></NavBar>
+    
+          <Main className={styles.main}>
+              <h2>
+                  404 | Not Found
+              </h2>
+          </Main>
+      </div>
   )
 }
 
